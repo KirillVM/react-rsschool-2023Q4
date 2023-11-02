569131b21913387b4f03c5e1e23ae628c06931e0
Kirill <101171995+KirillVM@users.noreply.github.com>
2023-11-02 17:40:37 +0000

    refactor: update data component according to react functional component style

--- src/components/data-list/data/data.tsx
+++ src/components/data-list/data/data.tsx
@@ -1,4 +1,4 @@
-import { Component, ReactNode } from 'react';
+import { useState } from 'react';
 import { RickAndMortyResponseResult } from '../../../types/ram-interfaces';
 import './data.css';
 
@@ -6,48 +6,51 @@ type DataProps = {
   responseResult: RickAndMortyResponseResult;
 };
 
-type DataState = {
-  imgUrl: string;
+type CardData = {
+  imageUrl: string;
   name: string;
   status: string;
-  spacies: string;
+  species: string;
   type: string;
   gender: string;
 };
 
-export default class Data extends Component<DataProps, DataState> {
-  constructor(props: DataProps) {
-    super(props);
-    this.state = {
-      imgUrl: this.props.responseResult.image,
-      name: this.props.responseResult.name,
-      status: this.props.responseResult.status,
-      spacies: this.props.responseResult.species,
-      type: this.props.responseResult.type || 'unknown',
-      gender: this.props.responseResult.gender,
-    };
-  }
-
-  render(): ReactNode {
-    return (
-      <div className="data-wrapper">
-        <img src={this.state.imgUrl} alt="img" className={'character-img'} />
-        <ul>
-          {Object.entries(this.state).map(
-            (data: [string, string]): ReactNode => {
-              return data[0] !== 'imgUrl' ? (
-                <li key={`${data[0]}${data[1]}`}>
-                  {`${data[0].charAt(0).toUpperCase() + data[0].slice(1)}: ${
-                    data[1]
-                  }`}
-                </li>
-              ) : (
-                ''
-              );
-            }
-          )}
-        </ul>
-      </div>
-    );
-  }
+function getPartialResponse(
+  responseResult: RickAndMortyResponseResult
+): CardData {
+  const partialResData: CardData = {
+    imageUrl: responseResult.image,
+    name: responseResult.name,
+    status: responseResult.status,
+    species: responseResult.species,
+    type: responseResult.type ? responseResult.type : 'none',
+    gender: responseResult.gender,
+  };
+  return partialResData;
 }
+
+const Data = ({ responseResult }: DataProps): JSX.Element => {
+  const [cardData] = useState<CardData>(getPartialResponse(responseResult));
+  return (
+    <div className="data-wrapper">
+      <img src={cardData.imageUrl} alt="img" className={'character-img'} />
+      <ul>
+        {Object.entries(cardData).map(
+          (data: [string, string]): JSX.Element | '' => {
+            return data[0] !== 'imageUrl' ? (
+              <li key={`${data[0]}${data[1]}`}>
+                {`${data[0].charAt(0).toUpperCase() + data[0].slice(1)}: ${
+                  data[1]
+                }`}
+              </li>
+            ) : (
+              ''
+            );
+          }
+        )}
+      </ul>
+    </div>
+  );
+};
+
+export default Data;
