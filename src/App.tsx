03b15648a6531c272a1842b3d8e1a30cf22c8312
Kirill <101171995+KirillVM@users.noreply.github.com>
2023-11-03 09:42:30 +0000

    refactor: update App component according to react functional component style

--- src/App.tsx
+++ src/App.tsx
@@ -1,65 +1,30 @@
-import { Component, ReactNode } from 'react';
+import { useLayoutEffect, useState } from 'react';
 import './App.css';
 import SearchForm from './components/search-form/search-form';
 import { RickAndMortyResponse } from './types/ram-interfaces';
 import DataList from './components/data-list/data-list';
 import Loader from './components/loader/loader';
 
-type AppState = {
-  isError: boolean;
-  isLoading: boolean;
-  searchParams: string;
-  searchData: RickAndMortyResponse | null;
-};
+const App = (): JSX.Element => {
+  const [isErrorS, setIsErrorS] = useState<boolean>(false);
+  const [isLoadingS, setIsLoadingS] = useState<boolean>(false);
+  const [searchParamsS, setSearchParamsSS] = useState<string>(
+    localStorage.getItem('lastSearchRow') || ''
+  );
+  const [searchDataS, setSearchDataS] = useState<RickAndMortyResponse | null>(
+    null
+  );
 
-export default class App extends Component<Readonly<object>, AppState> {
-  constructor(props: Readonly<object>) {
-    super(props);
-    this.state = {
-      isError: false,
-      isLoading: false,
-      searchParams: localStorage.getItem('lastSearchRow') || '',
-      searchData: null,
-    };
-    this.handleSearchSubmit = this.handleSearchSubmit.bind(this);
+  if (isErrorS) {
+    throw new Error('My Error');
   }
 
-  componentDidMount(): void {
-    this.getData(this.state.searchParams);
-  }
-
-  handleSearchSubmit = async (name: string): Promise<void> => {
-    this.getData(name);
+  const errorThrow = (): void => {
+    setIsErrorS(true);
   };
 
-  render(): ReactNode {
-    if (this.state.isError) {
-      throw new Error('My Error');
-    }
-    return (
-      <>
-        <SearchForm value={'search'} handleSubmit={this.handleSearchSubmit} />
-        <button className={'error-button'} onClick={this.errorThrow}>
-          ThrowError
-        </button>
-        <hr></hr>
-        {this.state.isLoading ? (
-          <Loader />
-        ) : this.state.searchData ? (
-          <DataList response={this.state.searchData} />
-        ) : (
-          <p style={{fontSize: '2rem'}}>NO DATA. PLEASE INSERT ANOTHER SEARCH PARAMETHER</p>
-        )}
-      </>
-    );
-  }
-
-  errorThrow = (): void => {
-    this.setState({ isError: true });
-  };
-
-  getData = async (name: string): Promise<void> => {
-    this.setState({ isLoading: true });
+  const getData = async (name: string): Promise<void> => {
+    setIsLoadingS(true);
     setTimeout(async (): Promise<void> => {
       const getResponse = await fetch(
         `https://rickandmortyapi.com/api/character/?name=${name}`,
@@ -68,17 +33,38 @@ export default class App extends Component<Readonly<object>, AppState> {
         }
       ).catch((error: Error): void => console.log(error));
       if (getResponse && getResponse.status === 200) {
-        this.setState({
-          searchData: await getResponse.json(),
-          searchParams: name,
-          isLoading: false,
-        });
+        setSearchParamsSS(name);
+        setSearchDataS(await getResponse.json());
+        setIsLoadingS(false);
       } else {
-        this.setState({
-          searchData: null,
-          isLoading: false,
-        });
+        setSearchDataS(null);
+        setIsLoadingS(false);
       }
     }, 3000);
   };
-}
+
+  useLayoutEffect(() => {
+    getData(searchParamsS);
+  });
+
+  return (
+    <>
+      <SearchForm value={'search'} handleSubmit={getData} />
+      <button className={'error-button'} onClick={errorThrow}>
+        ThrowError
+      </button>
+      <hr></hr>
+      {isLoadingS ? (
+        <Loader />
+      ) : searchDataS ? (
+        <DataList response={searchDataS} />
+      ) : (
+        <p style={{ fontSize: '2rem' }}>
+          NO DATA. PLEASE INSERT ANOTHER SEARCH PARAMETHER
+        </p>
+      )}
+    </>
+  );
+};
+
+export default App;
--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -5,14 +5,14 @@ type ButtonProps = {
   className: string[];
 };
 
-const Button = ({className}: ButtonProps): React.JSX.Element => {
-    const [type, setType] = useState<'submit' | 'button' | 'reset' | undefined>('submit');
-    const [text, setText] = useState<string>('Search');
-    return (
-      <button className={className.join(' ')} type={type}>
-        {text}
-      </button>
-    );
-}
+const Button = ({ className }: ButtonProps): React.JSX.Element => {
+  const [type] = useState<'submit' | 'button' | 'reset' | undefined>('submit');
+  const [text] = useState<string>('Search');
+  return (
+    <button className={className.join(' ')} type={type}>
+      {text}
+    </button>
+  );
+};
 
 export default Button;
