3526400e688c382acaac19ce1d973f911b6d781c
KirillVM <forjustice55@gmail.com>
2023-11-11 17:00:13 +0300

    chore: install and config jest

---
+++ src/__tests__/data.test.tsx
@@ -0,0 +1,10 @@
+// import { render, screen } from "@testing-library/react";
+// import user from "@testing-library/user-event";
+// import Data from "@components/data-list/data/data";
+
+describe("Catalog", () => {
+  test('firstr test', () => {
+    //expect(await screen.findByRole("button")).;
+    expect(1+1).toBe(2);
+  })
+})
